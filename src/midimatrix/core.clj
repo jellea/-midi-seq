f5783924660844fd50ad9f40196f36f953676b4b
Jelle Akkerman <jelle@jelleakkerman.nl>
2014-06-26 21:55:19 +0200

    adding mouse drag

--- src/midimatrix/core.clj
+++ src/midimatrix/core.clj
@@ -3,29 +3,34 @@
     [overtone.studio.midi :refer :all]
     [quil.core :as q]))
 
-;(defonce midi-out (midi-find-connected-receivers "TR-8"))
+(defonce midi-out (midi-find-connected-receivers "TR-8"))
 
-(defonce midi-out (midi-find-connected-receivers "IAC Driver Bus 1"))
+;(defonce midi-out (midi-find-connected-receivers "IAC Driver Bus 1"))
 
 (defn setup []
   (q/smooth)
   (q/frame-rate 30)
   (q/background 255))
 
+(defn gen-cells []
+  (vec (take 32 (repeat 0))))
+
 (def grid
   (atom [
-         {:step 0 :seq [1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0] :length 15 :name "BD" :midi-note 36}
-         {:step 0 :seq [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] :length 15 :name "SD" :midi-note 38}
-         {:step 0 :seq [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] :length 8  :name "LT" :midi-note 43}
-         {:step 0 :seq [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] :length 9  :name "MT" :midi-note 47}
-         {:step 0 :seq [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] :length 8  :name "HT" :midi-note 40}
-         {:step 0 :seq [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] :length 8  :name "RS" :midi-note 37}
-         {:step 0 :seq [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] :length 7  :name "HC" :midi-note 39}
-         {:step 0 :seq [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] :length 11 :name "CH" :midi-note 42}
-         {:step 0 :seq [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] :length 2  :name "OH" :midi-note 46}
-         {:step 0 :seq [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] :length 2  :name "CC" :midi-note 49}
-         {:step 0 :seq [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] :length 2  :name "RC" :midi-note 51}
+         {:step 0 :seq [1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 1 0] :length 15 :name "BD" :midi-note 36}
+         {:step 0 :seq (gen-cells) :length 15 :name "SD" :midi-note 38}
+         {:step 0 :seq (gen-cells) :length 15 :name "LT" :midi-note 43}
+         {:step 0 :seq (gen-cells) :length 15 :name "MT" :midi-note 47}
+         {:step 0 :seq (gen-cells) :length 15 :name "HT" :midi-note 40}
+         {:step 0 :seq (gen-cells) :length 15 :name "RS" :midi-note 37}
+         {:step 0 :seq (gen-cells) :length 15 :name "HC" :midi-note 39}
+         {:step 0 :seq (gen-cells) :length 15 :name "CH" :midi-note 42}
+         {:step 0 :seq (gen-cells) :length 15 :name "OH" :midi-note 46}
+         {:step 0 :seq (gen-cells) :length 15 :name "CC" :midi-note 49}
+         {:step 0 :seq (gen-cells) :length 15 :name "RC" :midi-note 51}
         ]))
+
+(def last-clicked (atom [100 100]))
 
 (defn advance-step [grid]
   (for [row grid]
@@ -39,7 +44,6 @@
   (cond
     (= status :step) (q/fill (q/color 255 0 0))
     (= status :step-on) (q/fill (q/color 205 80 255))
-
     (= status :disabled) (q/fill 50)
     (= status :disabled-on) (q/fill 90)
     (= status :on) (q/fill 170)
@@ -89,13 +93,21 @@
         y (int (- (Math/round (float (/ (q/mouse-x) 30))) 2))
         cell (get-in (vec @grid) [x :seq y])
         toggled-value (if (> cell 0) 0 100)]
+    (reset! last-clicked [x y])
     (if (q/key-pressed?)
       (reset! grid (assoc-in (vec @grid) [x :length] y))
       (reset! grid (assoc-in (vec @grid) [x :seq y] toggled-value)))))
+
+(defn drag-mouse []
+  (let [x (int (- (Math/round (float (/ (q/mouse-y) 30))) 1))
+        y (int (- (Math/round (float (/ (q/mouse-x) 30))) 2))]
+    (when (not (= @last-clicked [x y]))
+      (toggle-button))))
 
 (q/defsketch example
   :title ":midi-seq"
   :setup setup
   :draw draw
   :mouse-pressed toggle-button
-  :size [540 355])
+  :mouse-dragged drag-mouse
+  :size [1020 355])
